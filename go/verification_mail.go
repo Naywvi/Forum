f42e934d47e1d77976fab8fc39afd09e74c9d03f
NLAKHDARI <nagib.lakhdari@ynov.com>
2022-06-04 23:36:18 +0200

    add new default user & hash pswd during init bdd

--- go/verification_mail.go
+++ go/verification_mail.go
@@ -1,65 +1,10 @@
 //forumnlt@gmail.com	12301230789Aa
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-)
-
 //strings.TrimSpace(VARIABLE) <-- remove tabulation
 
 //#------------------------------------------------------------------------------------------------------------# ↓ init mail and hash password ↓
 // Call in BDD.go & send hash password
-func Terminal_Init_Table(who_whant string) string {
-
-	fmt.Println("---------------------")
-
-	if who_whant == "add_user_table" {
-
-		fmt.Println("Create a default user to connect on the web site.")
-		fmt.Print("username ->")
-
-		username, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-		username = strings.TrimSpace(username)
-
-		fmt.Print("email ->")
-
-		mail, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-		mail = strings.TrimSpace(mail)
-
-		fmt.Print("password ->")
-
-		pswd, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-		pswd = strings.TrimSpace(pswd)
-		pswd, _ = HashPassword(pswd)
-
-		fmt.Println("---------------------")
-
-		return "'" + username + "','" + pswd + "','none_desc','" + mail + "','none_picture','3'"
-
-	} else if who_whant == "email_verification_table" {
-
-		fmt.Println("Write your mail to send request register.")
-		fmt.Print("email ->")
-
-		mail, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-		mail = strings.TrimSpace(mail)
-
-		fmt.Print("password -> ")
-
-		pswd, _ := bufio.NewReader(os.Stdin).ReadString('\n') //<-- Email need \n to connect
-		pswd = strings.TrimSpace(pswd)
-		pswd, _ = HashPassword(pswd)
-
-		fmt.Println("---------------------")
-
-		return "'" + mail + "','" + pswd + "'"
-	}
-
-	return ""
-}
 
 //#------------------------------------------------------------------------------------------------------------# ↓ Send mail Register ↓
 // func SendMailRegister() {
